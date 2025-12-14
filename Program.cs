9fe30078195f283162bc9017f47414147884f226
Y0ungRebel <ilja.krivencko@yandex.ru>
2025-12-14 21:15:11 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace second_lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1st
+            //Console.Write("Введите число: ");
+            //int value = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите цифру: ");
+            //int c = Convert.ToInt32(Console.ReadLine());
+            //int n = 0;
+            //while (n <= 0)
+            //{
+            //    if (value % 10 == c) n++;
+            //    value /= 10;
+            //}
+            //if (n > 0) Console.WriteLine("Указанная цифра входит в заданное число");
+
+
+
+            //2nd
+            //Console.Write("Введите число: ");
+            //int value = Convert.ToInt32(Console.ReadLine());
+            //int count = value.ToString().Length;
+            //string result = "";
+            //while (result.Length != count)
+            //{
+            //    result += (value % 10).ToString();
+            //    value /= 10;
+            //}
+            //Console.WriteLine(result);
+
+
+
+            //3rd
+            //Console.Write("Введите число: ");
+            //int value = Convert.ToInt32(Console.ReadLine());
+            //int f = 1;
+            //int s = 1;
+            //int newVal = 0;
+            //Console.Write($"{f}, {s}");
+            //while (newVal < value)
+            //{
+            //    newVal = f + s;
+            //    f = s;
+            //    s = newVal;
+            //    Console.Write($", {newVal}");
+            //}
+            //if (value == newVal) Console.WriteLine("\nУказанное число является числом Фиббоначи");
+            //else Console.WriteLine("\nУказанное число не является числом Фиббоначи");
+        }
+    }
+}
