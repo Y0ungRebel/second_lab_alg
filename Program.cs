74af94b8f1f73621c75748e43dd69d33e1d54acf
Y0ungRebel <ilja.krivencko@yandex.ru>
2025-12-16 01:41:12 +0300

    upd

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,50 +12,14 @@ namespace second_lab
         static void Main(string[] args)
         {
             //1st
-            //Console.Write("Введите число: ");
-            //int value = Convert.ToInt32(Console.ReadLine());
-            //Console.Write("Введите цифру: ");
-            //int c = Convert.ToInt32(Console.ReadLine());
-            //int n = 0;
-            //while (n <= 0)
-            //{
-            //    if (value % 10 == c) n++;
-            //    value /= 10;
-            //}
-            //if (n > 0) Console.WriteLine("Указанная цифра входит в заданное число");
-
-
+            Console.WriteLine($"Указанная цифра входит в заданное число: {Loops.entryDigitInNumber(324, 3)}");
+            //var result = Loops.entryDigitInNumber(324, 3);
 
             //2nd
-            //Console.Write("Введите число: ");
-            //int value = Convert.ToInt32(Console.ReadLine());
-            //int count = value.ToString().Length;
-            //string result = "";
-            //while (result.Length != count)
-            //{
-            //    result += (value % 10).ToString();
-            //    value /= 10;
-            //}
-            //Console.WriteLine(result);
-
-
+            Console.WriteLine($"{Loops.reverseNumber(32452)}");
 
             //3rd
-            //Console.Write("Введите число: ");
-            //int value = Convert.ToInt32(Console.ReadLine());
-            //int f = 1;
-            //int s = 1;
-            //int newVal = 0;
-            //Console.Write($"{f}, {s}");
-            //while (newVal < value)
-            //{
-            //    newVal = f + s;
-            //    f = s;
-            //    s = newVal;
-            //    Console.Write($", {newVal}");
-            //}
-            //if (value == newVal) Console.WriteLine("\nУказанное число является числом Фиббоначи");
-            //else Console.WriteLine("\nУказанное число не является числом Фиббоначи");
+            Console.WriteLine($"Числом Фиббоначи: {Loops.isFibbonachi(55)}");
         }
     }
 }
